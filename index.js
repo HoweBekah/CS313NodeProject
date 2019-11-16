91d8b52de1d16e854cee89f5eb2d9b1778433d45
HoweBekah <how18019@byui.edu>
2019-11-16 00:13:12 -0700

    the form is made and the switch statement might work

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
-  .get("/cool", (req, res) => res.send(cool()))
-  .get("/math", function(req, res) {
+  .get("/postal", function(req, res) {
     console.log("Function called!");
-    let op1 = Number(req.query.op1);
-    let op2 = Number(req.query.op2);
-    console.log(op1);
-    console.log(op2);
-    var operator = req.query.operator;
-    console.log(operator);
+    let weight = Number(req.query.weight);
+    var mailType = req.query.mailType;
+    console.log(mailType);
     var result = 0;
-    switch (operator) {
-      case "+":
-        console.log("Addition!");
-        result = op1 + op2;
+    switch (mailType) {
+      case "stamped":
+        console.log("stamped!");
+        //result = op1 + op2;
         break;
-      case "-":
-        console.log("Subtraction!");
-        result = op1 - op2;
+      case "metered":
+        console.log("metered");
+        // result = op1 - op2;
         break;
-      case "/":
-        console.log("Division!");
-        result = op1 / op2;
+      case "envelope":
+        console.log("envelope");
+        //result = op1 / op2;
         break;
-      case "*":
-        console.log("Multiplication!");
-        result = op1 * op2;
+      case "package":
+        console.log("package");
+        //result = op1 * op2;
         break;
       default:
         console.log("Broken!");
         break;
     }
-    console.log(result);
-    res.render("pages/results", {
-      result: result
-    });
+    // console.log(result);
+    // res.render("pages/results", {
+    //   result: result
+    // });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
